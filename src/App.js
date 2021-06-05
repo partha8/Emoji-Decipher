44a4ed968bdc8e58b3555fb4784aeccd872aa2a7
Partha Pratim Sarma <64675650+partha8@users.noreply.github.com>
2021-06-05 17:35:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,162 @@
+import { useState } from "react";
+import "./styles.css";
+
+const database = {
+  "😀": "Grinning Face",
+  "😃": "Grinning Face with Big Eyes",
+  "😄": "Grinning Face with Smiling Eyes",
+  "😁": "Beaming Face with Smiling Eyes",
+  "😆": "Grinning Squinting Face",
+  "😅": "Grinning Face with Sweat",
+  "🤣": "Rolling on the Floor Laughing",
+  "😂": "Face with Tears of Joy",
+  "🙂": "Slightly Smiling Face",
+  "🙃": "Upside-Down Face",
+  "😉": "Winking Face",
+  "😊": "Smiling Face with Smiling Eyes",
+  "😇": "Smiling Face with Halo",
+  "🥰": "Smiling Face with Hearts",
+  "😍": "Smiling Face with Heart-Eyes",
+  "🤩": "Star-Struck",
+  "😘": "Face Blowing a Kiss",
+  "😗": "Kissing Face",
+  "😚": "Kissing Face with Closed Eyes",
+  "😙": "Kissing Face with Smiling Eyes",
+  "😋": "Face Savoring Food",
+  "😛": "Face with Tongue",
+  "😜": "Winking Face with Tongue",
+  "🤪": "Zany Face",
+  "😝": "Squinting Face with Tongue",
+  "🤑": "Money-Mouth Face",
+  "🤗": "Hugging Face",
+  "🤭": "Face with Hand Over Mouth",
+  "🤫": "Shushing Face",
+  "🤔": "Thinking Face",
+  "🤐": "Zipper-Mouth Face",
+  "🤨": "Face with Raised Eyebrow",
+  "😐": "Neutral Face",
+  "😑": "Expressionless Face",
+  "😶": "Face Without Mouth",
+  "😏": "Smirking Face",
+  "😒": "Unamused Face",
+  "🙄": "Face with Rolling Eyes",
+  "😬": "Grimacing Face",
+  "🤥": "Lying Face",
+  "😌": "Relieved Face",
+  "😔": "Pensive Face",
+  "😪": "Sleepy Face",
+  "🤤": "Drooling Face",
+  "😴": "Sleeping Face",
+  "😷": "Face with Medical Mask",
+  "🤒": "Face with Thermometer",
+  "🤕": "Face with Head-Bandage",
+  "🤢": "Nauseated Face",
+  "🤮": "Face Vomiting",
+  "🤧": "Sneezing Face",
+  "🥵": "Hot Face",
+  "🥶": "Cold Face",
+  "🥴": "Woozy Face",
+  "😵": "Dizzy Face",
+  "🤯": "Exploding Head",
+  "🤠": "Cowboy Hat Face",
+  "🥳": "Partying Face",
+  "😎": "Smiling Face with Sunglasses",
+  "🤓": "Nerd Face",
+  "🧐": "Face with Monocle",
+  "😕": "Confused Face",
+  "😟": "Worried Face",
+  "🙁": "Slightly Frowning Face",
+  "😮": "Face with Open Mouth",
+  "😯": "Hushed Face",
+  "😲": "Astonished Face",
+  "😳": "Flushed Face",
+  "🥺": "Pleading Face",
+  "😦": "Frowning Face with Open Mouth",
+  "😧": "Anguished Face",
+  "😨": "Fearful Face",
+  "😰": "Anxious Face with Sweat",
+  "😥": "Sad but Relieved Face",
+  "😢": "Crying Face",
+  "😭": "Loudly Crying Face",
+  "😱": "Face Screaming in Fear",
+  "😖": "Confounded Face",
+  "😣": "Persevering Face",
+  "😞": "Disappointed Face",
+  "😓": "Downcast Face with Sweat",
+  "😩": "Weary Face",
+  "😫": "Tired Face",
+  "🥱": "Yawning Face",
+  "😤": "Face with Steam From Nose",
+  "😡": "Pouting Face",
+  "😠": "Angry Face",
+  "🤬": "Face with Symbols on Mouth",
+  "😈": "Smiling Face with Horns",
+  "👿": "Angry Face with Horns"
+};
+
+export default function App() {
+  const emojis = Object.keys(database);
+  const [meaning, setMeaning] = useState("");
+  function changeHandler(e) {
+    if (e.target.value === "") {
+      setMeaning("");
+      return;
+    }
+    console.log(e.target.value);
+    var input = e.target.value;
+    var meaning = database[input];
+    if (meaning === undefined) {
+      meaning = "Sorry we dont have this in our database";
+      setMeaning(meaning);
+    } else {
+      setMeaning(meaning);
+    }
+  }
+  function clickHandler(emoji) {
+    var meaning = database[emoji];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <header>
+        <h1>Emoji Decipher</h1>
+        <small>
+          by{" "}
+          <a
+            style={{ color: "white" }}
+            href="https://twitter.com/partha_sarma8"
+            target="_blank"
+          >
+            Partha
+          </a>
+        </small>
+      </header>
+      <div className="container">
+        <h2>
+          Enter an emoji or select one from below to know the emoji's meaning
+        </h2>
+        <input onChange={changeHandler}></input>
+        <h2> {meaning} </h2>
+      </div>
+
+      <a
+        style={{ textDecoration: "none", scrollBehavior: "smooth" }}
+        href="#top"
+      >
+        <div className="emojis-container">
+          {emojis.map((emoji, i) => {
+            return (
+              <button
+                onClick={() => clickHandler(emoji)}
+                className="emoji-container"
+                key={emoji}
+              >
+                {emoji}
+              </button>
+            );
+          })}
+        </div>
+      </a>
+    </div>
+  );
+}
